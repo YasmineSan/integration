bbd15c84f17f9b6eefb3e5ebb785d810b82d3176
YasmineSan <yasmine-sanchez@proton.me>
2024-08-21 10:50:55 +0200

    fix export

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ module.exports = {
   webpack: (cfg) => {
     cfg.module.rules.push({
       test: /\.md$/,
-      output: "export",
       loader: "frontmatter-markdown-loader",
       options: { mode: ["react-component"] },
     });
