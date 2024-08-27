1b746f5ae4c72510c8ad1108332f0f338941ec87
yasmineSan <yasmine-sanchez@proton.me>
2024-08-27 14:39:45 +0200

    feat: Add lazy loading for images in shop page and update color scheme

--- components/BeerHighlight.js
+++ components/BeerHighlight.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import classNames from 'classnames';
 
 const BeerHighlight = ({ beerName, imageUrl, gradientColors }) => {
+  const gradientClass = classNames(
+    'bg-gradient-to-r',
+    {
+      'from-white to-black': gradientColors[0] === '#ffffff' && gradientColors[1] === '#000000',
+      'from-flori-red to-flori-dark-red': gradientColors[0] === '#FB0303' && gradientColors[1] === '#950202',
+      // Ajoutez d'autres combinaisons de classes ici si nécessaire
+    }
+  );
+
   return (
-    <div className="bg-gradient-to-r from-flori-red to-flori-dark-red rounded-lg max-w-md mx-auto">
+    <div className={`${gradientClass} rounded-lg max-w-md mx-auto`}>
       <div className="flex flex-col items-center shadow-lg hover:shadow-2xl transition-shadow duration-300 rounded">
-        <img src={imageUrl} alt={beerName} className="w-full h-auto rounded-t-lg" />
+      <img src={imageUrl} alt={beerName} className="w-full h-64 object-cover rounded-t-lg" />
         <h2 className="text-white text-3xl font-dancing font-bold my-4">{beerName}</h2>
-        <button className="bg-[#FB0303] text-white font-bold p-2 mb-4 rounded-full hover:bg-white hover:text-[#FB0303] transition">
+        <button className="bg-[#FB0303] text-white font-bold p-2 my-4 rounded-full hover:bg-white hover:text-[#FB0303] transition">
           <a href='/shop'>Go to Shop</a>
         </button>
       </div>
--- pages/shop.js
+++ pages/shop.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import BeerHighlight from '../components/BeerHighlight'
 
-export default function shop() {
+export default function Shop() {
   return (
-    <div>
+    <div className='py-4'>
         <Navbar />
         <img src='\img\top-view-beer-frame-with-copy-space.jpg' />
         <h1 className='font-kameron text-xl-4 sm:text-xs'>Shop</h1>
+        <div className="space-y-8"> {/* Ajout de `space-y-8` pour espacer les cartes verticalement */}
+          <BeerHighlight
+            beerName="Souvenir"
+            imageUrl="\img\453505478_122186157674024765_6170764990246105489_n.jpg"
+            gradientColors={['#FB0303', '#950202']}
+          />
+          <BeerHighlight
+            beerName="L'autre"
+            imageUrl="\img\top-view-beer-frame-with-copy-space.jpg"
+            gradientColors={['#ffffff', '#000000']}
+          />
+        </div>
     </div>
   )
 }
