98f8a995ce49bd8fafb3687e3e2f43a5ed9ac785
YasmineSan <yasmine-sanchez@proton.me>
2024-08-28 14:38:45 +0200

    feat: Add lazy loading for images in home and shop pages

--- pages/_test.js
+++ pages/_test.js
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import React from "react";
 
 export default function ParallaxFooter() {
   const [scrollY, setScrollY] = useState(0);
@@ -18,11 +19,27 @@ export default function ParallaxFooter() {
 
   return (
     <>
-     <Navbar />
-    <section className="bg-blue-500 text-white p-6">
-      <p>Quelque chose</p>
+      <Navbar />
+      <section className="relative h-screen">
+        <video
+          className="absolute top-0 left-0 w-full h-full object-cover z-0"
+          src="\img\vecteezy_beer-beer-foam-beer-bubbles-light-beer_39404101.mp4"
+          autoPlay
+          loop
+          muted
+        />
+        <div className="relative z-10 bg-red-500 bg-opacity-50 text-white p-6 flex justify-center items-center h-full">
+          <p className="text-3xl font-bold">
+            Chez Floribière, nous étions loin d'imaginer l'aventure que pouvait
+            représenter la création d'une bière au coquelicot. C'est au prix
+            d'une remise en question permanente que nous progressons.
+          </p>
+        </div>
       </section>
-    <Footer />
+      <section className="bg-blue-500 text-white p-6">
+        <p>Quelque chose</p>
+      </section>
+      <Footer />
     </>
   );
 }
--- pages/home.js
+++ pages/home.js
@@ -11,31 +11,30 @@ export default function HomePage() {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
- 
+
   return (
     <div>
       <Navbar />
       <div className="relative">
-  <img
-    src="https://scontent.fbru2-1.fna.fbcdn.net/v/t39.30808-6/438906167_122169957182024765_3362404178392777280_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_ohc=nMebbGTWM7cQ7kNvgHjd6Zt&_nc_ht=scontent.fbru2-1.fna&oh=00_AYAsOOSEpl3RZrW8Ng8C3dzKAVlRZ0y4wWjhPXET8_b27g&oe=66D2B948"
-    alt="Laissons fleurir nos bières"
-    className="w-full h-screen"
-  />
-  
-</div>
-<div className=" inset-0 flex items-center justify-center">
-    <h2 className="font-dancing italic text-center text-xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-8xl">
-      Laissons fleurir nos rêves.
-    </h2>
-  </div>
-  <div className="p-4">
-      <BeerHighlight
-        beerName="Floribière -  Souvenir"
-        imageUrl="\img\453505478_122186157674024765_6170764990246105489_n.jpg"
-        gradientColors={['#FB0303', '#950202']}
-      />
-    </div>
-    
+        <img
+          src="\img\Idée 1biere.png"
+          alt="Laissons fleurir nos bières"
+          className="w-full h-screen"
+        />
+      </div>
+      <div className=" inset-0 flex items-center justify-center">
+        <h2 className="font-dancing italic text-center text-xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-8xl">
+          Laissons fleurir nos rêves.
+        </h2>
+      </div>
+      <div className="p-4">
+        <BeerHighlight
+          beerName="Floribière -  Souvenir"
+          imageUrl="\img\453505478_122186157674024765_6170764990246105489_n.jpg"
+          gradientColors={["#FB0303", "#950202"]}
+        />
+      </div>
+
       <div className="bg-blue-500 text-white p-6">
         <h1 className="text-4xl font-bold">Welcome to My Static Homepage</h1>
         <p className="mt-4">This content is not editable through the CMS.</p>
